58ebd63a4b256a1b1b36f5de29b40e7b57445540
yyfxm <yangyafangxm@163.com>
2018-03-11 11:04:18 +0800

    爬取知乎

---
+++ zhihu_user/zhihu_user/spiders/zhihu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Spider,Request
+
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+    def start_requests(self):
+        url = "https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20"
+        yield Request(url,callback=self.parse)
+    def parse(self, response):
+        print(response.text)
